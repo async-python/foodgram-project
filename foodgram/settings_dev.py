4bd250a4786b99a6855627099b9025e0fec1c794
vardeath <vardeath@yandex.ru>
2021-06-21 17:03:24 +0300

    iter3-fix

--- foodgram/settings_dev.py
+++ foodgram/settings_dev.py
@@ -1,135 +1,135 @@
-import os
-from pathlib import Path
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
-SITE_ID = 1
-
-INSTALLED_APPS = [
-    'api',
-    'recipes',
-    'users',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'debug_toolbar',
-    'django_extensions',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'multiselectfield',
-    'sorl.thumbnail',
-    'rest_framework',
-    'django_filters',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-]
-
-ROOT_URLCONF = 'foodgram.urls'
-
-TEMPLATES_DIR = BASE_DIR / 'templates'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR, ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'recipes.processors.purchases_processor',
-                'recipes.processors.all_tags',
-                'django.template.context_processors.request',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'foodgram.wsgi.application'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'NumericPasswordValidator',
-    },
-]
-
-REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS':
-        ['django_filters.rest_framework.DjangoFilterBackend']
-}
-
-LANGUAGE_CODE = 'ru'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [BASE_DIR / 'assets', ]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-INTERNAL_IPS = ['127.0.0.1']
-
-LOGIN_URL = "/auth/login/"
-LOGIN_REDIRECT_URL = "index"
-
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
-
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-
-SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
-
-PAGINATOR_ITEMS_DISPLAY = 3
+# import os
+# from pathlib import Path
+#
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+#
+# BASE_DIR = Path(__file__).resolve().parent.parent
+#
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+#
+# DEBUG = True
+#
+# ALLOWED_HOSTS = ['*']
+#
+# SITE_ID = 1
+#
+# INSTALLED_APPS = [
+#     'api',
+#     'recipes',
+#     'users',
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     'debug_toolbar',
+#     'django_extensions',
+#     'django.contrib.sites',
+#     'django.contrib.flatpages',
+#     'multiselectfield',
+#     'sorl.thumbnail',
+#     'rest_framework',
+#     'django_filters',
+# ]
+#
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# ]
+#
+# ROOT_URLCONF = 'foodgram.urls'
+#
+# TEMPLATES_DIR = BASE_DIR / 'templates'
+#
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [TEMPLATES_DIR, ],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#                 'recipes.processors.purchases_processor',
+#                 'recipes.processors.all_tags',
+#                 'django.template.context_processors.request',
+#             ],
+#         },
+#     },
+# ]
+#
+# WSGI_APPLICATION = 'foodgram.wsgi.application'
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+#
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.'
+#                 'UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.'
+#                 'MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.'
+#                 'CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.'
+#                 'NumericPasswordValidator',
+#     },
+# ]
+#
+# REST_FRAMEWORK = {
+#     'DEFAULT_FILTER_BACKENDS':
+#         ['django_filters.rest_framework.DjangoFilterBackend']
+# }
+#
+# LANGUAGE_CODE = 'ru'
+#
+# TIME_ZONE = 'UTC'
+#
+# USE_I18N = True
+#
+# USE_L10N = True
+#
+# USE_TZ = True
+#
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [BASE_DIR / 'assets', ]
+#
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#
+# INTERNAL_IPS = ['127.0.0.1']
+#
+# LOGIN_URL = "/auth/login/"
+# LOGIN_REDIRECT_URL = "index"
+#
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+#
+# EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+#
+# DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+#
+# SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
+#
+# PAGINATOR_ITEMS_DISPLAY = 3
